b28eeddaf0dfa4d0563093b58f4d19541fc694a8
KevinDruart <65339848+KevinDruart@users.noreply.github.com>
2021-06-25 00:08:12 +0200

    maj

--- backend/app.js
+++ backend/app.js
@@ -3,13 +3,17 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const Thing = require('./models/things');
+
 const saucesRoutes = require('./routes/sauces');
+const userRoutes = require('./routes/user');
 
 mongoose.connect('mongodb+srv://userAdmin:gzdQHZbGu233g8c7@cluster0.oziog.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 const app = express();
 
@@ -18,10 +22,11 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
-  });
+});
 
-  app.use(bodyParser.json());
+app.use(bodyParser.json());
 
-  app.use('/api/sauces', saucesRoutes);
+app.use('/api/sauces', saucesRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -1,4 +1,4 @@
-const Thing = require('../models/thing');
+const Thing = require('../models/things');
 
 exports.createThing = (req, res, next) => {
   const thing = new Thing({
@@ -28,3 +28,78 @@ exports.createThing = (req, res, next) => {
     }
   );
 };
+
+exports.getOneThing = (req, res, next) => {
+    Thing.findOne({
+      _id: req.params.id
+    }).then(
+      (thing) => {
+        res.status(200).json(thing);
+      }
+    ).catch(
+      (error) => {
+        res.status(404).json({
+          error: error
+        });
+      }
+    );
+  };
+  
+  exports.modifyThing = (req, res, next) => {
+    const thing = new Thing({
+        userId: req.body.userId,
+        name: req.body.name,
+        manufacturer: req.body.manufacturer,
+        description: req.body.description,
+        mainPepper: req.body.mainPepper,
+        imageUrl: req.body.imageUrl,
+        heat: req.body.heat,
+        likes: req.body.likes,
+        dislikes: req.body.dislikes,
+        userLiked: req.body.userLiked,
+        userDisliked: req.body.userDisliked
+    });
+    Thing.updateOne({_id: req.params.id}, thing).then(
+      () => {
+        res.status(201).json({
+          message: 'Sauce modifié avec succés!'
+        });
+      }
+    ).catch(
+      (error) => {
+        res.status(400).json({
+          error: error
+        });
+      }
+    );
+  };
+  
+  exports.deleteThing = (req, res, next) => {
+    Thing.deleteOne({_id: req.params.id}).then(
+      () => {
+        res.status(200).json({
+          message: 'Sauce supprimer!'
+        });
+      }
+    ).catch(
+      (error) => {
+        res.status(400).json({
+          error: error
+        });
+      }
+    );
+  };
+  
+  exports.getAllSauces = (req, res, next) => {
+    Thing.find().then(
+      (things) => {
+        res.status(200).json(things);
+      }
+    ).catch(
+      (error) => {
+        res.status(400).json({
+          error: error
+        });
+      }
+    );
+  };
