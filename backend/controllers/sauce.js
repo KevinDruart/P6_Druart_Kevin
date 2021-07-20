88ea9fd53787cee544d8c6f1686fa68163c00d8c
KevinDruart <65339848+KevinDruart@users.noreply.github.com>
2021-07-20 11:48:31 +0200

    controlleur likeDislike, test en cours

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -5,30 +5,30 @@ exports.createSauce = (req, res, next) => {
   const sauceObject = JSON.parse(req.body.sauce);
   //delete sauceObject._id; 
   const sauce = new SauceModele({
-      ...sauceObject, 
-      imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
-      likes: 0,
-      dislikes: 0,
+    ...sauceObject,
+    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
+    likes: 0,
+    dislikes: 0,
   });
   sauce.save()
-      .then(() => res.status(201).json({ message: 'Sauce enregistrée' }))
-      .catch(error => { 
-        console.log(error);
-        res.status(400).json({ error })
-      });
+    .then(() => res.status(201).json({ message: 'Sauce enregistrée' }))
+    .catch(error => {
+      console.log(error);
+      res.status(400).json({ error })
+    });
 };
 
 exports.getOneSauce = (req, res, next) => {
   SauceModele.findOne({
     _id: req.params.id
   })
-  .then((sauce) => { res.status(200).json(sauce); })
-  .catch((error) => { 
-    console.log("erreur recherche 1 sauce")
-    res.status(404).json({ error: error }); 
-  })
+    .then((sauce) => { res.status(200).json(sauce); })
+    .catch((error) => {
+      console.log("erreur recherche 1 sauce")
+      res.status(404).json({ error: error });
+    })
 };
-  
+
 exports.modifySauce = (req, res, next) => {
   const sauceObject = req.file ?
     {
@@ -42,7 +42,7 @@ exports.modifySauce = (req, res, next) => {
       res.status(400).json({ error })
     });
 };
-  
+
 exports.deleteSauce = (req, res, next) => {
   SauceModele.findOne({ _id: req.params.id })
     .then(sauce => {
@@ -61,17 +61,70 @@ exports.deleteSauce = (req, res, next) => {
       res.status(500).json({ error })
     });
 };
-  
+
 exports.getAllSauce = (req, res, next) => {
   SauceModele.find()
-  .then(
-    (sauce) => { res.status(200).json(sauce); })
+    .then(
+      (sauce) => { res.status(200).json(sauce); })
     .catch(
-    (error) => {
-      console.log("erreur recherche toutes les sauces"); 
-      res.status(400).json({ error: error }); })
+      (error) => {
+        console.log("erreur recherche toutes les sauces");
+        res.status(400).json({ error: error });
+      })
 };
 
 exports.likeDislike = (req, res, next) => {
+  // Like présent dans le body
+  let like = req.body.like
+  // On prend le userID
+  let userId = req.body.userId
+  // On prend l'id de la sauce
+  let sauceId = req.params.id
 
+  // S'il clique j'aime(On push l'utilisateur et on incrémente le compteur like de 1)
+  if (like === 1) {
+    Sauce.updateOne({
+      _id: sauceId
+    }, {
+      // On push l'utilisateur
+      $push: {
+        usersLiked: userId
+      },
+      // On incrémente de 1 like avec l'operateur $inc
+      $inc: {
+        likes: +1
+      },
+    })
+      .then(() => res.status(200).json({
+        message: 'j\'aime ajouté !'
+      }))
+      .catch((error) => res.status(400).json({
+        error
+      }))
+  }
+  // S'il clique je n'aime pas (On push l'utilisateur et on incrémente le compteur dislike de 1)
+  if (like === -1) {
+    Sauce.updateOne(
+      {
+        _id: sauceId
+      }, {
+      // On push l'utilisateur
+      $push: {
+        usersDisliked: userId
+      },
+      // On incrémente de 1 dislike avec l'operateur $inc
+      $inc: {
+        dislikes: +1
+      },
+    }
+    )
+      .then(() => {
+        res.status(200).json({
+          message: 'Dislike ajouté !'
+        })
+      })
+      .catch((error) => res.status(400).json({
+        error
+      }))
+  }
 }
