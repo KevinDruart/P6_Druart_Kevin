4c87cd616a7585ce46310ae0dad6baa06542cdf2
KevinDruart <65339848+KevinDruart@users.noreply.github.com>
2021-08-11 15:40:24 +0200

    maj maskage email

--- backend/middleware/updateVerifSauce.js
+++ backend/middleware/updateVerifSauce.js
@@ -2,8 +2,15 @@ const fs = require('fs');
 
 module.exports = (req, res, next) => {
 	try {
+
 		//on enregistre la sauce dans une variable
-		let sauce = JSON.parse(req.body.sauce);
+		let sauce =  null;/* req.file ? JSON.parse(req.body.sauce) : req.body*/
+		if(req.file){
+			sauce = JSON.parse(req.body.sauce);
+		}
+		else {
+			sauce = req.body;
+		}
 		console.log(sauce);
 
 		// enlever les espaces en début et fin de string
@@ -12,9 +19,11 @@ module.exports = (req, res, next) => {
 		let description = sauce.description.trim();
 		let mainPepper = sauce.mainPepper.trim();
 
+		console.log("ligne 21");
+
 		// Vérifier que les champs texte soient remplis
 		if (name.length > 0 && manufacturer.length > 0 && description.length > 0 && mainPepper.length > 0) {
-
+			console.log("ligne 26");
 			// si tout est ok, on récupère les champs sans les espaces
 			sauce.name = name;
 			sauce.manufacturer = manufacturer;
@@ -29,8 +38,10 @@ module.exports = (req, res, next) => {
 				!regexSauce.test(sauce.description) ||
 				!regexSauce.test(sauce.mainPepper)
 			) {
+				console.log("ligne 41");
 				res.status(400).json({ error:"Veillez à n'utiliser que des chiffres, des lettres et les caractères , . -" });
 			} else {
+				console.log("ligne 44");
 				// et on peut passer la requête au prochain middleware
 				req.body.sauce = JSON.stringify(sauce);
 				//on passe au middleware suivant
@@ -38,11 +49,13 @@ module.exports = (req, res, next) => {
 			}
 
 		} else {
+			console.log("ligne 52");
 			// sinon on supprime la nouvelle image que multer a déjà sauvegardé
 			fs.unlink(`images/${req.file.filename}`, () => {
 				res.status(400).json({ error: 'Requête non valable, l\'image a été supprimée' });
 			})
 		}
+
 	} catch (error) {
 		console.log(error);
 		res.status(400).json({ error: "modification impossible" });
